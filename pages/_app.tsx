e49a47ad717d67043d17af03ec502fea370d80b7
maruma <maruyama_001@jimusuru.info>
2025-07-01 00:27:10 +0900

    feat: show chatbot widget only on homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import type { AppProps } from 'next/app';
 
 import '../styles/global.css';
-import ChatBotWidget from '../components/ChatBotWidget';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <ChatBotWidget />
     </>
   );
 }
