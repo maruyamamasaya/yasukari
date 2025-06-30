2d2f8d14fc57157643bd58ade646aaf894ab8686
maruma <maruyama_001@jimusuru.info>
2025-06-30 22:24:48 +0900

    Add global chat widget

---
+++ components/ChatBotWidget.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { FaComments } from "react-icons/fa";
+import ChatBot from "./ChatBot";
+
+export default function ChatBotWidget() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="fixed bottom-4 right-4 z-50">
+      {open ? (
+        <div className="relative">
+          <button
+            onClick={() => setOpen(false)}
+            className="absolute -top-3 -right-3 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center"
+          >
+            ×
+          </button>
+          <ChatBot />
+        </div>
+      ) : (
+        <button
+          onClick={() => setOpen(true)}
+          className="bg-blue-600 text-white p-3 rounded-full shadow-lg"
+        >
+          <FaComments className="w-5 h-5" />
+        </button>
+      )}
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app';
 
 import '../styles/global.css';
+import ChatBotWidget from '../components/ChatBotWidget';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ChatBotWidget />
+    </>
+  );
 }
