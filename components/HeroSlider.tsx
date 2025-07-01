eabddf6af6b2a2462d1ac05b84856886819cd207
maruma <maruyama_001@jimusuru.info>
2025-07-01 14:42:57 +0900

    Add navigation to HeroSlider

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/navigation";
 
 export default function HeroSlider({ slides }: { slides: { img: string }[] }) {
   return (
     <section className="relative flex items-center justify-center" style={{ height: 560 }}>
-      <Swiper modules={[Autoplay]} autoplay={{ delay: 4000 }} loop className="absolute inset-0">
+      <Swiper
+        modules={[Autoplay, Navigation]}
+        navigation
+        autoplay={{ delay: 4000 }}
+        loop
+        className="absolute inset-0"
+      >
         {slides.map((s, idx) => (
           <SwiperSlide key={idx}>
             <img src={s.img} alt="slide" width={960} height={560} className="w-full h-full object-cover" />
