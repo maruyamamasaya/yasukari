c9ba361811b543efafdf008b279a13609d0e1bec
maruma <maruyama_001@jimusuru.info>
2025-07-01 14:09:35 +0900

    Add genre pages and update links

---
+++ pages/t/genre/cc251-400.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function CC251to400Page() {
+  return (
+    <div className="p-6 text-center text-sm">
+      <Head>
+        <title>251〜400cc - yasukari</title>
+      </Head>
+      <h1 className="text-xl font-bold mb-4">251〜400cc</h1>
+      <p className="mb-6">
+        掲載されている車種の写真はイメージとしての参考例です。実際にご提供する車両は、色や年式などが異なる場合がございますので、あらかじめご了承ください。
+      </p>
+      <p>このカテゴリーの詳細ページは現在準備中です。</p>
+    </div>
+  );
+}
---
+++ pages/t/genre/scooter-50cc.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Scooter50ccPage() {
+  return (
+    <div className="p-6 text-center text-sm">
+      <Head>
+        <title>原付スクーター - yasukari</title>
+      </Head>
+      <h1 className="text-xl font-bold mb-4">原付スクーター</h1>
+      <p className="mb-6">
+        掲載されている車種の写真はイメージとしての参考例です。実際にご提供する車両は、色や年式などが異なる場合がございますので、あらかじめご了承ください。
+      </p>
+      <p>このカテゴリーの詳細ページは現在準備中です。</p>
+    </div>
+  );
+}
